20372f454b2880d4d76e4313532630bfdc181f49
Anupam Kumar <37176796+anupamkumar2243@users.noreply.github.com>
2022-04-26 18:48:06 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,14 @@
+import tesserocr
+from PIL import Image
+
+class ocr:
+    def __init__(self, filename):
+        self.filename = filename
+
+
+
+    def getPrediction(self):
+        image = Image.open(self.filename)
+        lines = tesserocr.image_to_text(image)  
+        return lines
+
